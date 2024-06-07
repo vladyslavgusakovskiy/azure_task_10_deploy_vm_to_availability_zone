1bf1706eea2441ad62c683c7bc1a1b9a03e663dd
Bohdan Mateiko <bohdan.id27182@gmail.com>
2024-06-07 11:33:03 +0300

    updates tests code to remove case sensitivity for script validation

--- scripts/test-code.ps1
+++ scripts/test-code.ps1
@@ -1,36 +1,36 @@
 $scriptContent = Get-Content ./task.ps1
 
-if ($scriptContent | Where-Object {$_.Contains("New-AzResourceGroup")}) { 
+if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azresourcegroup")}) { 
     Write-Host "Checking if script creates a resource group - ok" 
 } else { 
     throw "Script is not creating a resource group, please review it. "
 } 
 
-if ($scriptContent | Where-Object {$_.Contains("New-AzNetworkSecurityGroup")}) { 
+if ($scriptContent | Where-Object {$_.ToLower().Contains("new-aznetworksecuritygroup")}) { 
     Write-Host "Checking if script creates a network security group - ok" 
 } else { 
     throw "Script is not creating a network security group, please review it. "
 } 
 
-if ($scriptContent | Where-Object {$_.Contains("New-AzVirtualNetwork")}) { 
+if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azvirtualnetwork")}) { 
     Write-Host "Checking if script creates a virtual network - ok" 
 } else { 
     throw "Script is not creating a virtual network, please review it. "
 } 
 
-if ($scriptContent | Where-Object {$_.Contains("New-AzSshKey")}) {
+if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azsshkey")}) {
     Write-Host "Checking if script creates a SSH key resource - ok" 
 } else { 
     throw "Script is not creating a SSH key resource, please review it. "
 } 
 
-if ($scriptContent | Where-Object {$_.Contains("New-AzVm")}) {
+if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azvm")}) {
     Write-Host "Checking if script creates a VM resource - ok" 
 } else { 
     throw "Script is not creating a VM resource, please review it. "
 } 
 
-if ($scriptContent | Where-Object {$_.Contains("-Zone")}) {
+if ($scriptContent | Where-Object {$_.ToLower().Contains("-zone")}) {
     Write-Host "Checking if script has a zone assignment - ok" 
 } else { 
     throw "Script does not contain a zone assignment for the VM creation, please review it. "
