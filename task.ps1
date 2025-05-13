469bf4034fe1c2000755a1213b9ab3bb7a54ea67
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-05-13 14:28:20 +0300

    Create two VMs and set them to different availability zones

--- task.ps1
+++ task.ps1
@@ -6,8 +6,7 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
-$vmName = "matebox"
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
 
@@ -33,14 +32,25 @@ New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey 
 # and set same zone you would set on the VM, but this is not required in this task. 
 # New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel "random32987"
 
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $vmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $subnetName `
--VirtualNetworkName $virtualNetworkName `
--SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName 
-# -PublicIpAddressName $publicIpAddressName
+function Create-VM {
+    param (
+        [string]$vmName,
+        [string]$zone
+    )
+    New-AzVm `
+    -ResourceGroupName $resourceGroupName `
+    -Name $vmName `
+    -Location $location `
+    -image $vmImage `
+    -size $vmSize `
+    -Zone $zone `
+    -SubnetName $subnetName `
+    -VirtualNetworkName $virtualNetworkName `
+    -SecurityGroupName $networkSecurityGroupName `
+    -SshKeyName $sshKeyName
+    # -PublicIpAddressName $publicIpAddressName
+
+}
+
+Create-Vm -vmName "matebox" -zone "1"
+Create-Vm -vmName "matebox2" -zone "2"
